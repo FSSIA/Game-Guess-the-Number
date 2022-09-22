1b14f5bd92a221c860dc529423c82d86b4319248
Sergejs Tiselskis <slokas85@gmail.con>
2022-09-22 09:38:11 +0300

    Initial Commit

---
+++ Game Guess the Number/Game_Guess_the_NumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_Guess_the_NumberApp.swift
+//  Game Guess the Number
+//
+//  Created by John Doe on 22/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Game_Guess_the_NumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
